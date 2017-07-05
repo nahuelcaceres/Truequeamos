0bae54ba2ce1599f2c2881ee5ccb5f8f8257b65c
nahuelcaceres <nahuelcaceres@users.noreply.github.com>
2017-07-04 23:31:28 -0300

    Add files via upload

---
+++ Truequeamos/src/Trueque.java
@@ -0,0 +1,17 @@
+/**
+ * Created by nahuel on 04/07/2017.
+ */
+public class Trueque {
+    private double importe;
+
+    public Trueque(){
+    }
+
+    public double Importe(){
+        return importe;
+    }
+
+    public boolean ExisteUsuario(int usuario){
+        return (usuario == 1);
+    }
+}
